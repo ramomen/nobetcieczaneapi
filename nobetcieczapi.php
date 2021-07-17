d90dfcdb3378d40031ae962734a462ab0b2814db
Ramazan Değirmenci <45834008+ramomen@users.noreply.github.com>
2021-07-17 10:41:35 +0300

    Update nobetcieczapi.php

--- nobetcieczapi.php
+++ nobetcieczapi.php
@@ -5,12 +5,20 @@ function ara($bas, $son, $yazi)
       '(.*?)'. preg_quote($son, '/').'/i', $yazi, $m);
       return @$m[1];
     }
-    $sehir = 'konya';
-    $ilce = 'seydisehir';
+    $sehir = 'ankara';
+    $ilce = 'etimesgut';
     $link = "https://nobet.org/$sehir/$ilce/nobetci-eczaneler.html";
 
     $icerik = file_get_contents($link);
     $goster = ara('<span>','</span>',$icerik);
-    echo '<h2> '.$goster[12].' Eczanesi<br/> Adres = '.$goster[13].' </br>  Telefon: '.$goster[14].'</h2>';
+    
+    $eczane = [
+    "eczane" => $goster[12],
+     "Adres" => $goster[13],
+     "Telefon" =>$goster[14]
+    ]
+        
+        return json($eczane);
+   // echo '<h2> '.$goster[12].' Eczanesi<br/> Adres = '.$goster[13].' </br>  Telefon: '.$goster[14].'</h2>';
 
     ?>
