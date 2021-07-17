672077554067b42fd54092808fe04622add85e88
Ramazan Değirmenci <ramazan.degirmenci@egbasoft.com>
2021-07-17 10:55:10 +0300

    edited something and added json_array for "eczane" but guess  our host shutdown :(

--- README_LAN_BENI.md
+++
@@ -1,8 +0,0 @@
-# nobetcieczaneapi
-file_get_content functionını kullanarak.
-
-fark ettiysen kullandığım function web content içerisindeki dataları array'lere bölüyor. her böldüğünede [burasına] numara atıyor. sende bunu .$falanca[bilmemkaç] ile kullanabilir printleyebilir ya da echolayabilirsin.
-
-
-eyyorlamam bu kadardır hade hayırlı işler
-AKA rich nigga
--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+header('Content-type: application/json');
+
 function ara($bas, $son, $yazi)
     {
       @preg_match_all('/' . preg_quote($bas, '/') .
@@ -12,13 +14,13 @@ function ara($bas, $son, $yazi)
     $icerik = file_get_contents($link);
     $goster = ara('<span>','</span>',$icerik);
     
-    $eczane = [
-    "eczane" => $goster[12],
-     "Adres" => $goster[13],
-     "Telefon" =>$goster[14]
-    ]
-        
-        return json($eczane);
-   // echo '<h2> '.$goster[12].' Eczanesi<br/> Adres = '.$goster[13].' </br>  Telefon: '.$goster[14].'</h2>';
+    $eczane = array(
+      "eczane" => $goster[12],
+      "Adres" => $goster[13],
+      "Telefon" =>$goster[14]
+    );
+    
+  
+    echo json_encode( $eczane );
 
     ?>
